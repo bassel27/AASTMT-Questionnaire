ad39fa8e2b99a208dd0686c02c591cce3f8a9b76
Bassel Attia <bassel27@Bassels-MacBook-Air-2.local>
2023-12-04 22:06:58 +0200

    initial commit

---
+++ constants.py
@@ -0,0 +1,9 @@
+PORTAL_URL = r'https://studentportal.aast.edu/'
+RADIO_BUTTONS={
+    "أوافق تماما"	: '//input[@value="ctl00"]',
+    "متوسط" :'//input[@value="ctl04"]',
+    "لا أوافق تماما" : '//input[@value="ctl08"]'
+}
+
+username = ""
+password = ""
